7bf1f9f674c5a19e2b2a896d27bb22e4cc3aaa5c
alexsummers <alexsummers@yandex.ua>
2013-05-26 23:10:00 +0300

    Изменена функция hashSearch(anyHash, hashKey)

--- search.rb
+++ search.rb
@@ -4,31 +4,47 @@ module Search
 		puts("\n\tЗадание 4\n Описание задания:\nПроизводит в Hash поиск значения по заданному ключу.")
 	end
 
-	def self.hashSearch(hashVar, hashKey)
+	def self.hashSearch(anyHash, hashKey)# поиск по ключу, если ключ не найден возвращает NilClass
 	
-		if hashVar.is_a?(Hash) || hashVar.is_a?(Array)
-			hashVar.each{|key, value|
-				if key == hashKey
-					@@res = value
-					return @@res
-				elsif key.is_a?(Hash) or key.is_a?(Array)
-					@@res = hashSearch(key, hashKey)
-				else value.is_a?(Hash) or value.is_a?(Array)
-					@@res = hashSearch(value, hashKey)
+		if anyHash.is_a?(Hash) or anyHash.is_a?(Array)
+	
+			anyHash.each{|key, value|
+			
+				if key.to_s == hashKey.to_s
+				
+					if value == nil
+					puts ("\n\nНайденное значение по ключу #{key} равно nil")# значение найдено но равно nil
+					end
+					
+				 	return @res = value 
+
+				else
+					if key.is_a?(Hash) or key.is_a?(Array)# проверяем ключ и значение
+					
+						@res = hashSearch(key, hashKey)
+						
+					else value.is_a?(Hash) or value.is_a?(Array)
+					
+						@res = hashSearch(value, hashKey)
+						
+					end
+					
 				end
 			}
-			@@res = "\t Ключа \'#{hashKey}\' нет в заданном \'#{hashVar.class}\':\n #{hashVar}"
-		else
-		@@res = "\tОшибка !!! Заданная переменная типа \'#{hashVar.class}\'!\nНеобходимо указать переменную типа \'Hash\' или \'Array\'.\nПоиск значения по ключу \"#{hashKey}\" невозможен!"
 		end
+		
+		return @res
 		
 	end
 	
 	def self.runApp #выполнение задания
-		@@has = { 'a' => :X1, 'b' => :X2, 'c' => [{"ya" => :X3}, {"yb" => :X4}]}
+		@@has = {"test"=>1,proc{}=>[[{:key=>"sss"},{"pass"=>{"a"=> [1,{:bsd => [[],[],[],[],[],[[proc{}] => {[7,1]=>{:rr=>111}}]]}] }}], [1,[:olol, {[1, {:trolo => "r"}] => 1}]]],"zero"=>7,"kuk"=>[{"tr"=>"jxp"},{:lx=>"xp"}]}
 		description
 		print("\n\tРезультат выполнения задания:\n#{@@has}\n Введите значение ключа:")
-		puts("Ключю #{tmp = gets.chomp} соответствует значение #{hashSearch(@@has, 'yb')}\n")
+		key = gets.chomp
+		res = hashSearch(@@has, key)
+		puts("\nКлючю \'#{key}\' значение #{"не" if res==nil } найдено#{", оно равно:\n#{res}" if res!=nil}")
   end
 
 end
+Search::runApp
